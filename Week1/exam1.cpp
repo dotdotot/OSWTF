4e2921e9b8856e28b212af389ff7a2ef52650459
dotdotot <dotdotot203@naver.com>
2022-01-07 19:03:52 +0900

    완전제곱수 찾기

--- Week1/exam1.cpp
+++ Week1/exam1.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int algorithm()
+{   
+    int maxNum = 100;
+    int count = 0;
+    for (int i = 1; i <= maxNum; i++)
+    {
+        int totalSquareNum = i*i;
+        if(totalSquareNum > maxNum){
+            break;
+        }
+        count++;
+    }
 
+    return count;
+}
+
+int main()
+{
+    int count = algorithm();
+    cout << count;
     return 0;
 }
